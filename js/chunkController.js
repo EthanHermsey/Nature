fbfeaffc8ea3b80bc3327bec39cfd71c52c91005
EthanHermsey <ivanhermse@gmail.com>
2023-07-13 01:49:26 +0200

    fix: set default viewdistance

--- js/chunkController.js
+++ js/chunkController.js
@@ -8,8 +8,8 @@ class ChunkController {
 		this.createNewChunks = {};
 		this.castChunks = [];
 		this.prevCoord = undefined;
-		this.chunkViewDistance = 4;
-		this.farChunkEdge = 2;
+		this.chunkViewDistance = 6;
+		this.farChunkEdge = 4;
         this.totalViewDistance =  this.chunkViewDistance + this.farChunkEdge;
 
 		this.grassViewDistance = 4;
